3ef846c037dd68e44ce6437091a0fd8ac1cf658a
Shreya Agrawal <84276060+ag-sh1996@users.noreply.github.com>
2024-04-04 18:27:38 -0500

    Create Problem3.java

---
+++ Problem3.java
@@ -0,0 +1,29 @@
+public class Solution {
+    /**
+     * @param words: a list of words
+     * @param word1: a string
+     * @param word2: a string
+     * @return: the shortest distance between word1 and word2 in the list
+     */
+     //TC: O(n)
+     //Sc: O(1)
+    public int shortestDistance(String[] words, String word1, String word2) {
+        // Write your code here'
+        if(words == null) return 0;
+        int i1 = -1, i2 = -1;
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i <words.length; i++){
+            if(words[i].equals(word1)) i1 = i;
+            if(words[i].equals(word2)){
+                if(i1 == i){
+                    i1 = i2;
+                }
+                i2 = i;
+            }
+            if(i1 != -1 && i2 != -1){
+                min = Math.min(min,Math.abs(i2-i1));
+            }
+        }
+        return min;
+     }
+}
